69fdceed4834404bc43b7a46fc71a9a9550ad5db
Mattias Gulec <Voltair88@gmail.com>
2022-03-31 15:52:52 +0200

    end of day 2022-03-31

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,12 @@
 import React from 'react';
 import './App.css';
-import { getAuth, signInAnonymously } from 'firebase/auth';
-import { firebaseConfig } from './firebase';
-import { initializeApp } from 'firebase/app';
-
-initializeApp(firebaseConfig);
-
-const auth = getAuth();
-signInAnonymously(auth)
-  .then(() => {
-    console.log(auth.currentUser);
-    // Signed in.. test 4
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ...
-    console.log(errorCode, errorMessage);
-  });
+import Username from './Components/username';
+import { auth } from './Firebase/firebase';
 
 function App() {
   return (
     <div className='App'>
-      <p>Welcome to React!</p>
+      <Username />
     </div>
   );
 }
