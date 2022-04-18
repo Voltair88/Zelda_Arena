03458cfaa5b5882a52f037bfbb4796076e90dd73
Voltair88 <Voltair88@gmail.com>
2022-04-18 16:06:35 +0200

    fix: working on the map

--- src/Firebase/firebase.tsx
+++ src/Firebase/firebase.tsx
@@ -7,9 +7,9 @@ import {
   set,
   onDisconnect,
   onValue,
+  onChildAdded,
 } from 'firebase/database';
 import characterName from '../Components/characterName';
-import { createRoot } from 'react-dom/client';
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
@@ -18,58 +18,91 @@ const auth = getAuth(firebaseApp);
 // communicate with the database
 const database = getDatabase(firebaseApp);
 
-function initGame() {
-  const allPlayersRef = ref(database, 'players');
+(function () {
+  let playerId: any;
+  let playerRef: any;
+  const players = {} as any;
+  const playerElements = {} as { [key: string]: HTMLElement };
 
-  // create a listener for allPlayerRef to listen for changes
-  onValue(allPlayersRef, (snapshot) => {
-    const allPlayers = snapshot.val();
-    const allPlayersArray = Object.keys(allPlayers).map((key) => {
-      return allPlayers[key];
+  const gameContainer = document.querySelector('.Game') as HTMLElement;
+
+  function initGame() {
+    const allPlayersRef = ref(database, 'players');
+
+    // create a listener for allPlayerRef to listen for changes
+
+    onValue(allPlayersRef, (snapshot) => {
+      const allPlayers = snapshot.val();
+      const allPlayersArray = Object.keys(allPlayers).map((key) => {
+        return allPlayers[key];
+      });
+      const allPlayersSorted = allPlayersArray.sort((a, b) => {
+        return b.score - a.score;
+      });
+
+      const highScores = document.getElementById('Highscore') as HTMLElement;
+      highScores.innerHTML = '';
+      allPlayersSorted.forEach((player) => {
+        const playerElement = document.createElement('div');
+        playerElement.innerHTML = `${player.name}`;
+        highScores.appendChild(playerElement);
+      });
     });
-    const allPlayersSorted = allPlayersArray.sort((a, b) => {
-      return b.score - a.score;
+
+    // Create a new player
+    onChildAdded(allPlayersRef, (snapshot) => {
+      const addedPlayer = snapshot.val();
+      const characterElement = document.createElement('div');
+      characterElement.classList.add('character', 'grid-cell');
+      if (addedPlayer.id === playerId) {
+        characterElement.classList.add('you');
+      }
+      characterElement.innerHTML = `
+          <div class="Character_shadow grid-cell"></div>
+          <div class="Character_sprite grid-cell"></div>
+          <div class="Character_name-container">
+            <span class="Character_name"></span>
+          </div>
+          <div class="Character_you-arrow"></div>
+        `;
+      playerElements[addedPlayer.id] = characterElement as HTMLElement;
+
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      characterElement.querySelector('.Character_name')!.innerHTML =
+        addedPlayer.name;
     });
-
-    const highScores = document.getElementById('Highscore') as HTMLElement;
-    highScores.innerHTML = '';
-    allPlayersSorted.forEach((player) => {
-      const playerElement = document.createElement('div');
-      playerElement.innerHTML = `${player.name}`;
-      highScores.appendChild(playerElement);
-    });
-  });
-}
-
-// sign in anonymously
-signInAnonymously(auth)
-  .then(() => {
-    console.log(auth.currentUser);
-    // Signed in..
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // An error happened.
-    console.log(errorCode, errorMessage);
-  });
-
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    //You're logged in!
-    const playerId = user.uid;
-    const playerRef = ref(database, `players/${playerId}`);
-    set(playerRef, {
-      name: characterName,
-      id: playerId,
-    });
-    //Remove the player from the database when they disconnect (e.g. close the app)
-    onDisconnect(playerRef).remove();
-
-    initGame();
-  } else {
-    // User is signed out.
   }
-});
+
+  // sign in anonymously
+  signInAnonymously(auth)
+    .then(() => {
+      console.log(auth.currentUser);
+      // Signed in..
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // An error happened.
+      console.log(errorCode, errorMessage);
+    });
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      //You're logged in!
+      const playerId = user.uid;
+      const playerRef = ref(database, `players/${playerId}`);
+      set(playerRef, {
+        name: characterName,
+        id: playerId,
+      });
+      //Remove the player from the database when they disconnect (e.g. close the app)
+      onDisconnect(playerRef).remove();
+
+      initGame();
+    } else {
+      // User is signed out.
+    }
+  });
+})();
 
 export { auth, database };
