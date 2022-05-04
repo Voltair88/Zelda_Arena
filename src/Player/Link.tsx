165bf57356ca071b2f4e2d0ca84b93e2d377b6e3
Voltair88 <Voltair88@gmail.com>
2022-05-04 14:17:40 +0200

    added damage collision between player and enemy

--- src/Player/Link.tsx
+++ src/Player/Link.tsx
@@ -14,24 +14,159 @@ declare global {
   }
 }
 
-enum Direction {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT,
+enum HealthState {
+  HEALTHY,
+  DAMAGE,
+  DEAD,
 }
 
 export default class Link extends Phaser.Physics.Arcade.Sprite {
-  private direction = Direction.DOWN;
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+  private healthState: HealthState = HealthState.HEALTHY;
+  private damageTime = 0;
 
+  private _health = 3;
+
+  get health() {
+    return this._health;
+  }
   constructor(
     scene: Phaser.Scene,
     x: number,
     y: number,
     texture: string,
-    frame: string
+    frame?: string
   ) {
     super(scene, x, y, texture, frame);
+  }
+  handleDamage(dir: Phaser.Math.Vector2) {
+    if (this._health <= 0) {
+      return;
+    }
+
+    if (this.healthState === HealthState.DAMAGE) {
+      return;
+    }
+
+    --this._health;
+
+    if (this._health <= 0) {
+      // TODO: die
+      this.healthState = HealthState.DEAD;
+      // add animation
+      this.setVelocity(0, 0);
+    } else {
+      this.setVelocity(dir.x, dir.y);
+
+      this.setTint(0xff0000);
+
+      this.healthState = HealthState.DAMAGE;
+      this.damageTime = 0;
+    }
+  }
+
+  preUpdate(time: number, delta: number): void {
+    super.preUpdate(time, delta);
+
+    switch (this.healthState) {
+      case HealthState.HEALTHY:
+        break;
+      case HealthState.DAMAGE:
+        this.damageTime += delta;
+        if (this.damageTime >= 250) {
+          this.healthState = HealthState.HEALTHY;
+          this.setTint(0xffffff);
+          this.damageTime = 0;
+        }
+        break;
+    }
+  }
+
+  public update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+    const up = cursors.up?.isDown;
+    const down = cursors.down?.isDown;
+    const left = cursors.left?.isDown;
+    const right = cursors.right?.isDown;
+    const bow = cursors.space.isDown;
+    const speed = 100;
+
+    if (up) {
+      this.anims.play('walk-up', true);
+      this.setVelocity(0, -speed);
+    } else if (down) {
+      this.anims.play('walk-down', true);
+      this.setVelocity(0, speed);
+    } else if (left) {
+      this.anims.play('walk-left', true);
+      this.setVelocity(-speed, 0);
+    } else if (right) {
+      this.anims.play('walk-right', true);
+      this.setVelocity(speed, 0);
+    } else {
+      const parts = this.anims.currentAnim.key.split('-');
+      if (parts[1] === 'up') {
+        this.anims.play('idle-up', true);
+      } else if (parts[1] === 'down') {
+        this.anims.play('idle-down', true);
+      } else if (parts[1] === 'left') {
+        this.anims.play('idle-left', true);
+      } else if (parts[1] === 'right') {
+        this.anims.play('idle-right', true);
+      }
+      this.setVelocity(0, 0);
+    }
+    if (bow) {
+      if (
+        this.anims.currentAnim.key === 'walk-down' ||
+        this.anims.currentAnim.key === 'idle-down'
+      ) {
+        this.anims.play('bow-down', true).once('animationcomplete', () => {
+          this.anims.play('idle-down', true);
+        });
+      } else if (
+        this.anims.currentAnim.key === 'walk-up' ||
+        this.anims.currentAnim.key === 'idle-up'
+      ) {
+        this.anims.play('bow-up', true).once('animationcomplete', () => {
+          this.anims.play('idle-up', true);
+        });
+      } else if (
+        this.anims.currentAnim.key === 'walk-left' ||
+        this.anims.currentAnim.key === 'idle-left'
+      ) {
+        this.anims.play('bow-left', true).once('animationcomplete', () => {
+          this.anims.play('idle-left', true);
+        });
+      } else if (
+        this.anims.currentAnim.key === 'walk-right' ||
+        this.anims.currentAnim.key === 'idle-right'
+      ) {
+        this.anims.play('bow-right', true).once('animationcomplete', () => {
+          this.anims.play('idle-right', true);
+        });
+      }
+    }
+    if (
+      this.anims.currentAnim.key === 'bow-down' ||
+      this.anims.currentAnim.key === 'bow-up' ||
+      this.anims.currentAnim.key === 'bow-left' ||
+      this.anims.currentAnim.key === 'bow-right'
+    ) {
+      this.setOffset(8, 12);
+    } else {
+      this.setOffset(4, 16);
+    }
+
+    if (
+      this.healthState === HealthState.DAMAGE ||
+      this.healthState === HealthState.DEAD
+    ) {
+      return;
+    }
+
+    if (!cursors) {
+      return;
+    }
   }
 }
 
--- src/Scenes/Game.tsx
+++ src/Scenes/Game.tsx
@@ -17,14 +17,16 @@ import GreenSoldier from '../enemies/greenSoldier';
 export default class Game extends Phaser.Scene {
   private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
   private Link!: Link;
+  private greenSoldiers!: Phaser.Physics.Arcade.Group;
   private animatedTiles!: AnimatedTile[];
+  private PlayerEnemysCollision?: Phaser.Physics.Arcade.Collider;
+  private hit = 0;
 
   constructor() {
     super({ key: 'Game' });
   }
 
   public init(): void {
-    this.cursors = this.input.keyboard.createCursorKeys();
     this.animatedTiles = [];
   }
   public preload(): void {
@@ -50,12 +52,23 @@ export default class Game extends Phaser.Scene {
 
     // load in greenSoldier
     greenSoldierAnims(this.anims);
-    const greenSoldiers = this.physics.add.group({
+    this.greenSoldiers = this.physics.add.group({
       classType: GreenSoldier,
+      maxSize: 10,
+      runChildUpdate: true,
+      createCallback: (go) => {
+        const greenGo = go as GreenSoldier;
+        greenGo.body.onCollide = true;
+      },
     });
 
-    greenSoldiers.get(150, 150, 'green_soldier').setPushable(false).setMass(10);
-    greenSoldiers.get(150, 250, 'green_soldier').setPushable(false).setMass(10);
+    this.greenSoldiers.get(150, 150, 'green_soldier').setMass(10);
+    this.greenSoldiers.get(150, 250, 'green_soldier').setMass(10);
+
+    /*       const greenSoldierLayer = map.getObjectLayer('greenSoldier');
+      greenSoldierLayer.objects.forEach((obj) => {
+        this.greenSoldiers.get(obj.x, obj.y, 'green_soldier');
+      }); */
 
     // Load player animations
     playerAnims(this.anims);
@@ -79,11 +92,11 @@ export default class Game extends Phaser.Scene {
 
     this.physics.add.collider(this.Link, wallsLayer);
     this.physics.add.collider(this.Link, obstaclesLayer);
-    this.physics.add.collider(greenSoldiers, wallsLayer);
-    this.physics.add.collider(greenSoldiers, obstaclesLayer);
-    this.physics.add.collider(
+    this.physics.add.collider(this.greenSoldiers, wallsLayer);
+    this.physics.add.collider(this.greenSoldiers, obstaclesLayer);
+    this.PlayerEnemysCollision = this.physics.add.collider(
       this.Link,
-      greenSoldiers,
+      this.greenSoldiers,
       this.handlePlayerEnemyCollision,
       undefined,
       this
@@ -112,105 +125,37 @@ export default class Game extends Phaser.Scene {
     obj1: Phaser.GameObjects.GameObject,
     obj2: Phaser.GameObjects.GameObject
   ) {
+    const link = obj1 as Link;
     const GreenSoldier = obj2 as GreenSoldier;
 
-    const dx = this.Link.x - GreenSoldier.x;
-    const dy = this.Link.y - GreenSoldier.y;
+    console.log('collision');
 
-    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(100);
+    const dx = link.x - GreenSoldier.x;
+    const dy = link.y - GreenSoldier.y;
+
+    const dir = new Phaser.Math.Vector2(dx, dy).normalize().scale(200);
 
     this.Link.setVelocity(dir.x, dir.y);
+
+    this.hit += 1;
   }
 
   // create animated tiles
   // loop through every tile and check if its id is in the animated tile's array
 
-  public update(time: number, delta: number): void {
+  update(time: number, delta: number): void {
     this.animatedTiles.forEach((tile) => tile.update(delta));
 
-    if (this.input.keyboard) {
-      if (this.input.keyboard.addKey('W').isDown || this.cursors.up.isDown) {
-        this.Link.anims.play('walk-up', true);
-        this.Link.setVelocity(0, -100);
-      } else if (
-        this.input.keyboard.addKey('S').isDown ||
-        this.cursors.down.isDown
-      ) {
-        this.Link.anims.play('walk-down', true);
-        this.Link.setVelocity(0, 100);
-      } else if (
-        this.input.keyboard.addKey('A').isDown ||
-        this.cursors.left.isDown
-      ) {
-        this.Link.anims.play('walk-left', true);
-        this.Link.setVelocity(-100, 0);
-      } else if (
-        this.input.keyboard.addKey('D').isDown ||
-        this.cursors.right.isDown
-      ) {
-        this.Link.anims.play('walk-right', true);
-        this.Link.setVelocity(100, 0);
-      } else {
-        // put the Link in idle state
-        if (this.Link.anims.currentAnim.key === 'walk-down')
-          this.Link.anims.play('idle-down', true);
-        else if (this.Link.anims.currentAnim.key === 'walk-up')
-          this.Link.anims.play('idle-up', true);
-        else if (this.Link.anims.currentAnim.key === 'walk-left')
-          this.Link.anims.play('idle-left', true);
-        else if (this.Link.anims.currentAnim.key === 'walk-right')
-          this.Link.anims.play('idle-right', true);
-        this.Link.setVelocity(0, 0);
+    if (this.hit > 0) {
+      ++this.hit;
+      if (this.hit > 10) {
+        this.hit = 0;
       }
-      // when pressing E load the bow anims depending on the direction the player is facing
-      if (this.input.keyboard.addKey('E').isDown) {
-        if (
-          this.Link.anims.currentAnim.key === 'walk-down' ||
-          this.Link.anims.currentAnim.key === 'idle-down'
-        ) {
-          this.Link.anims
-            .play('bow-down', true)
-            .once('animationcomplete', () => {
-              this.Link.anims.play('idle-down', true);
-            });
-        } else if (
-          this.Link.anims.currentAnim.key === 'walk-up' ||
-          this.Link.anims.currentAnim.key === 'idle-up'
-        ) {
-          this.Link.anims.play('bow-up', true).once('animationcomplete', () => {
-            this.Link.anims.play('idle-up', true);
-          });
-        } else if (
-          this.Link.anims.currentAnim.key === 'walk-left' ||
-          this.Link.anims.currentAnim.key === 'idle-left'
-        ) {
-          this.Link.anims
-            .play('bow-left', true)
-            .once('animationcomplete', () => {
-              this.Link.anims.play('idle-left', true);
-            });
-        } else if (
-          this.Link.anims.currentAnim.key === 'walk-right' ||
-          this.Link.anims.currentAnim.key === 'idle-right'
-        ) {
-          this.Link.anims
-            .play('bow-right', true)
-            .once('animationcomplete', () => {
-              this.Link.anims.play('idle-right', true);
-            });
-        }
-      }
-      if (
-        this.Link.anims.currentAnim.key === 'bow-down' ||
-        this.Link.anims.currentAnim.key === 'bow-up' ||
-        this.Link.anims.currentAnim.key === 'bow-left' ||
-        this.Link.anims.currentAnim.key === 'bow-right'
-      ) {
-        this.Link.setVelocity(0, 0);
-        this.Link.setOffset(8, 12);
-      } else {
-        this.Link.setOffset(4, 16);
-      }
+      return;
+    }
+
+    if (this.Link) {
+      this.Link.update(this.cursors);
     }
   }
 }
