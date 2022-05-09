a28c20a1180d297d4d6cd0250f93709b5a5c3ed3
Voltair88 <Voltair88@gmail.com>
2022-05-09 23:40:46 +0200

    removed config to eject 2

--- config/webpack.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-module.exports = {
-  resolve: {
-    alias: {
-      Animations: path.resolve(__dirname, '../src/Animations'),
-      Event: path.resolve(__dirname, '../src/events'),
-    },
-  },
-};
