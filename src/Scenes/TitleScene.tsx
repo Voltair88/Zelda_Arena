3aa30e5cda92f08e5b161614c02456a8ea0297ca
Voltair88 <Voltair88@gmail.com>
2022-05-16 11:26:36 +0200

    done with the title screen

--- src/Scenes/TitleScene.tsx
+++ src/Scenes/TitleScene.tsx
@@ -9,19 +9,27 @@ export default class TitleScene extends Phaser.Scene {
 
   preload() {
     this.load.audio('titleScreenMusic', 'Assets/music/title_screen.ogg');
+    this.load.image('background', 'Assets/ui/titlescreen.png');
   }
   create() {
     this.titleScreenMusic = this.sound.add('titleScreenMusic', { volume: 0.5, loop: true });
+    this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
     this.titleScreenMusic.play();
-    this.add.text(this.game.renderer.width / 2.3 - 100, this.game.renderer.height / 2 - 200, 'Zelda Arena', {
-      fontSize: '64px',
-      color: '#0b0b0b',
-      backgroundColor: '#ffffff58',
+    this.add.text(this.game.renderer.width / 3 - 100, this.game.renderer.height / 2 - 220, 'Zelda Arena', {
+      fontFamily: 'Zelda',
+      fontSize: '150px',
+      color: '#e71837',
+      stroke: '#000000',
+      strokeThickness: 5,
     });
     const startButton = this.add.text(this.game.renderer.width / 2.3, this.game.renderer.height / 2, 'Start', {
-      fontSize: '64px',
-      color: '#0b0b0b',
-      backgroundColor: '#ffffff58',
+      fontFamily: 'Zelda',
+      fontSize: '90px',
+      fixedHeight: 100,
+      fixedWidth: 200,
+      color: '#ffe449',
+      stroke: '#000000',
+      strokeThickness: 5,
     });
 
     startButton.setInteractive();
@@ -29,6 +37,15 @@ export default class TitleScene extends Phaser.Scene {
       this.scene.start('Preloader');
       sceneEvents.emit('resetScore');
       this.titleScreenMusic?.stop();
+    });
+
+    this.tweens.add({
+      targets: startButton,
+      duration: 700,
+      ease: 'Cubin.easeInOut',
+      alpha: 0,
+      repeat: -1,
+      yoyo: true,
     });
   }
 }
