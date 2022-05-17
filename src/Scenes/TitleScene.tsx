b746db8f18165aaf58a7a725fbc339e35a35c2f7
Voltair88 <Voltair88@gmail.com>
2022-05-17 10:57:21 +0200

    updated the title screen

--- src/Scenes/TitleScene.tsx
+++ src/Scenes/TitleScene.tsx
@@ -10,19 +10,22 @@ export default class TitleScene extends Phaser.Scene {
   preload() {
     this.load.audio('titleScreenMusic', 'Assets/music/title_screen.ogg');
     this.load.image('background', 'Assets/ui/titlescreen.png');
+    this.load.image('titletext', 'Assets/ui/titletext.png');
   }
   create() {
     this.titleScreenMusic = this.sound.add('titleScreenMusic', { volume: 0.5, loop: true });
     this.add.image(this.cameras.main.width / 2, this.cameras.main.height / 2, 'background');
     this.titleScreenMusic.play();
-    this.add.text(this.game.renderer.width / 3 - 100, this.game.renderer.height / 2 - 220, 'Zelda Arena', {
+    const text = this.add.image(this.cameras.main.width / 2.2, this.cameras.main.height / 2 - 200, 'titletext');
+    text.setScale(0.35);
+    this.add.text(this.game.renderer.width / 2 - 250, this.game.renderer.height / 2 - 50, 'Arena', {
       fontFamily: 'Zelda',
-      fontSize: '150px',
+      fontSize: '90px',
       color: '#e71837',
       stroke: '#000000',
       strokeThickness: 5,
     });
-    const startButton = this.add.text(this.game.renderer.width / 2.3, this.game.renderer.height / 2, 'Start', {
+    const startButton = this.add.text(this.game.renderer.width / 2.3, this.game.renderer.height / 2 + 200, 'Start', {
       fontFamily: 'Zelda',
       fontSize: '90px',
       fixedHeight: 100,
